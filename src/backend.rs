66614f07a3e5de737c564c5c40d7940243758b0c
niels <33730531+yndolg@users.noreply.github.com>
2024-05-24 20:51:12 +0200

    Create dictionary

--- src/backend.rs
+++ src/backend.rs
@@ -1,10 +1,19 @@
+use std::ffi::{c_uint, c_void};
 use std::fmt::{Debug, Display};
+use std::string::String;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
 use glob::glob;
+use zstd::dict::{DDict, from_continuous};
 
+use zstd_sys;
 use tiktoken_rs::p50k_base;
 use zstd;
+use zstd_sys::ZDICT_trainFromBuffer;
 
+// https://wortschatz.uni-leipzig.de/en/download/English
 const DATA_PATH: &str = "./data";
+const DICT_SIZE_BYTES : usize = 1024 * 1024 * 1024;
 
 type Token = usize;
 
@@ -13,16 +22,60 @@ pub fn tokenize(str: &str) -> Vec<Token> {
     tokenizer.encode_with_special_tokens(str)
 }
 
-pub fn  compress(tokens: Vec<Token>) -> Vec<u8> {
-    zstd::encode_all(tokens.as)
+pub fn tokens_to_bytes(tokens: Vec<Token>) -> Vec<u8> {
+    tokens.iter().map(|x| x.to_le_bytes()).flatten().collect()
+}
+
+pub fn compress(tokens: Vec<Token>) {
+    let raw_data = tokens_to_bytes(tokens);
+    //zstd::encode_all(&raw_data, 0).unwrap()
 }
 
 
-fn load_datasets() -> Vec<String> {
+pub fn create_dictionary() {
+    let tokens = load_datasets();
+    let raw_data =  tokens_to_bytes(tokens);
+
+
+    let mut buffer = vec![0u8; DICT_SIZE_BYTES];
+
+    let sample_size = 1000;
+    let sizes_count = (raw_data.len() / sample_size) + 1;
+    let mut sizes = vec![sample_size; sizes_count];
+    sizes[sizes_count - 1] = raw_data.len() % sample_size;
+
+    unsafe {
+        ZDICT_trainFromBuffer(
+            buffer.as_mut_ptr() as *mut c_void,
+            DICT_SIZE_BYTES,
+            raw_data.as_ptr() as *mut c_void,
+            sizes.as_ptr(),
+            sizes.len() as c_uint
+        );
+    }
+
+
+    let dict = DDict::create(&buffer);
+    
+    // write buffer to file 
+    let mut file = File::create("model.zstd_dict").unwrap();
+    file.write_all(&buffer).unwrap();
+}
+
+fn load_datasets() -> Vec<Token> {
     find_datasets()
         .iter()
-        .map(|x| std::fs::read_to_string(x).unwrap())
-        .fold("", |acc, x| acc + &x)
+        .flat_map(|filename| tokenize_file(filename))
+        .collect()
+}
+
+fn tokenize_file(filename: &str) -> Vec<Token> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .flat_map(|x| tokenize(&x.expect("")))
         .collect()
 }
 
@@ -35,18 +88,22 @@ fn find_datasets() -> Vec<String> {
 }
 
 mod tests {
-    // https://wortschatz.uni-leipzig.de/en/download/English
-
-    use crate::backend::find_datasets;
+    use crate::backend::*;
 
     #[test]
     fn test_find_dataset() {
-        find_datasets().iter().for_each(|x| println!("Path found: {:?}", x));
+        find_datasets()
+            .iter()
+            .for_each(|x| println!("Path found: {:?}", x));
     }
 
     #[test]
     fn test_load_datasets() {
-        let datasets = crate::backend::load_datasets();
-        datasets.iter().for_each(|x| println!("Dataset: {:?}", x));
+        println!("Dataset: {:?}", load_datasets());
+    }
+    
+    #[test]
+    fn train_dict() {
+        create_dictionary();
     }
 }
