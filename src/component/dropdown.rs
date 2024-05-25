670037bb10d20d3b454fe0a60da5cdf3915bd017
TwentyFive <twentyfivesoftware@gmail.com>
2024-05-25 14:11:13 +0200

    Improve dropdown

--- src/component/dropdown.rs
+++ src/component/dropdown.rs
@@ -1,32 +1,30 @@
-use leptos::{Callback, component, create_signal, For, IntoView, Signal, SignalSet, SignalUpdate, view};
+use leptos::{component, create_signal, For, IntoView, ReadSignal, SignalSet, SignalUpdate, view, WriteSignal};
 
 #[component]
-pub fn Dropdown(
-    options: Vec<String>,
-    selected_option: Signal<String>,
-    set_selected_option: Callback<String>,
+pub fn Dropdown<const N: usize>(
+    options: [&'static str; N],
+    selected_option_index: ReadSignal<usize>,
+    set_selected_option_index: WriteSignal<usize>,
 ) -> impl IntoView {
     let (is_open, set_open) = create_signal(false);
 
     view! {
         <div class="dropdown">
             <div class="dropdown__selected" on:click=move |_| set_open.update(|is_currently_open| *is_currently_open = !*is_currently_open)>
-                <div>{selected_option}</div>
+                <div>{move || options[selected_option_index()]}</div>
                 <div class="dropdown__icon">></div>
             </div>
             <div class="dropdown__options" class=("hidden", move || !is_open())>
                 <For
-                    each=move || options.clone()
-                    key=|dropdown_option| dropdown_option.clone()
-                    children=move |dropdown_option| {
-                        let dropdown_option_clone= dropdown_option.clone();
-
+                    each=move || options.into_iter().enumerate()
+                    key=|(_, option)| *option
+                    children=move |(index, option)| {
                         view! {
                             <div class="dropdown__option" on:click=move |_| {
                                 set_open.set(false);
-                                set_selected_option(dropdown_option.clone());
+                                set_selected_option_index.set(index);
                             }>
-                                {dropdown_option_clone}
+                                {option}
                             </div>
                         }
                     }
--- src/component/navbar.rs
+++ src/component/navbar.rs
@@ -1,52 +1,37 @@
-use std::fmt::{Display, Formatter};
-use std::str::FromStr;
-use leptos::{Callback, component, create_signal, IntoView, Signal, view};
+use leptos::{component, create_signal, IntoView, view};
 use crate::component::dropdown::Dropdown;
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 enum Model {
     ChatCLM1_0,
     ChatGPT3_5,
     ChatGPT4o,
 }
 
-impl Display for Model {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+impl Model {
+    pub fn name(&self) -> &'static str {
         match self {
-            Model::ChatCLM1_0 => write!(f, "ChatCLM 1.0"),
-            Model::ChatGPT3_5 => write!(f, "ChatGPT 3.5"),
-            Model::ChatGPT4o => write!(f, "ChatGPT 4o"),
-        }
-    }
-}
-
-impl FromStr for Model {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "ChatCLM 1.0" => Ok(Model::ChatCLM1_0),
-            "ChatGPT 3.5" => Ok(Model::ChatGPT3_5),
-            "ChatGPT 4o" => Ok(Model::ChatGPT4o),
-            &_ => Err(()),
+            Model::ChatCLM1_0 => "ChatCLM 1.0",
+            Model::ChatGPT3_5 => "ChatGPT 3.5",
+            Model::ChatGPT4o => "ChatGPT 4o",
         }
     }
 }
 
 #[component]
 pub fn NavBar() -> impl IntoView {
-    let (selected_model, set_selected_model) = create_signal(Model::ChatCLM1_0);
+    let (selected_model_index, set_selected_model_index) = create_signal(0usize);
 
     view! {
         <nav class="navbar">
             <Dropdown
-                options=vec![
-                    Model::ChatCLM1_0.to_string(),
-                    Model::ChatGPT3_5.to_string(),
-                    Model::ChatGPT4o.to_string(),
+                options=[
+                    Model::ChatCLM1_0.name(),
+                    Model::ChatGPT3_5.name(),
+                    Model::ChatGPT4o.name(),
                 ]
-                selected_option=Signal::derive( move || selected_model().to_string())
-                set_selected_option=Callback::from(move |option: String| set_selected_model(Model::from_str(&option).unwrap()))
+                selected_option_index=selected_model_index
+                set_selected_option_index=set_selected_model_index
             />
         </nav>
     }
